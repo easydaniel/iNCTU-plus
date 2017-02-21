cc5b114511d99ab42b79533183b2c18c9fb9e718
Tocknicsu <wingemerald@gmail.com>
2017-02-21 18:41:34 +0800

    add eslint ignore and routing for web

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import Session from './Session';
+import { routerReducer as routing } from 'react-router-redux'
 
 export default combineReducers({
+  routing,
   Session,
 });
