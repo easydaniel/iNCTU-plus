4c7ad586169711d527d4b1d837960aa6b33acb07
Tocknicsu <wingemerald@gmail.com>
2017-02-23 00:47:02 +0800

    add course list and course time

---
+++ src/js/actions/Course.js
@@ -0,0 +1,9 @@
+import { createAction } from 'redux-actions';
+import Api from 'js/api';
+
+export default {
+  gets: createAction('GETS_COURSE', Api.Course.gets),
+  Time: {
+    get: createAction('GET_COURSE_TIME', Api.Course.Time.get),
+  }
+};
